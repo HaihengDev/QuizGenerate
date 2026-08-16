74bedcd57c428a2c5c782a6686f346c0677a05a1
HaihengDev <haihenglim9@gmail.com>
2026-08-16 12:56:15 +0700

    added: ResultPage component and updated routes configuration

--- src/config/RoutesConfig.tsx
+++ src/config/RoutesConfig.tsx
@@ -1,6 +1,7 @@
 import HomePage from '../pages/HomePage';
 import GenerateQuizForm from '../pages/GenerateQuizForm';
 import QuizForm from '../pages/QuizForm';
+import ResultPage from '../pages/ResultPage';
 import Error from '../pages/Error';
 
 export const ConfigRoutes = [
@@ -15,6 +16,10 @@ export const ConfigRoutes = [
   {
     path: '/quiz-form',
     element: <QuizForm />,
+  },
+  {
+    path: '/result',
+    element: <ResultPage />,
   },
   {
     path: '*',
---
+++ src/pages/ResultPage.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <h1>Result Page</h1>;
+};
+
+export default Page;
